7fd29940c916a089f8aee4cda4b50d6c78e22a6d
KirillHorn <kirilltihonov22@gmail.com>
2023-11-30 17:18:16 +0500

    Страница блюд сделана
    Исправить search элемент

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,17 +1,39 @@
 
 <x-x-header />
-    <section class="">
-        <div class="container">
-           
-        </div>
+    <section class="section_product_nav">
+        <div class="container d-flex flex-column gap-3" style="padding-bottom: 60px;">
+            <div class="directory_nav">
+                <a>Уфа</a>
+                <span>→</span>
+                <a>Блюда</a>
+            </div>
+                <div class="navigation d-flex flex-column gap-3">
+                        <div>
+                            <span>42 точки</span>
+                            <h2> Доставка еды из заведений Уфы</h2>
+                        </div>
+                            <div class="d-flex flex-column gap-4">
+                                <input type="search" class="search_content">
+                                    <div class="d-flex flex-wrap gap-4  a_content_categories">
+                                    <a>Бургеры</a> <a>Суши и роллы</a> <a>Пицца</a>
+                                    <a>Бургеры</a> <a>Суши и роллы</a> <a>Пицца</a>
+                                    <a>Бургеры</a> <a>Суши и роллы</a> <a>Пицца</a>
+                                    <a>Бургеры</a> <a>Суши и роллы</a> <a>Пицца</a>
+                                    <a>Бургеры</a> <a>Суши и роллы</a> <a>Пицца</a>
+                                    <a>Бургеры</a> <a>Суши и роллы</a> <a>Пицца</a>
+                                    <a>Бургеры</a> <a>Суши и роллы</a> 
+                                     </div>
+                             </div>
+                     </div>
+             </div>
     </section>
   
-        <section class="section_cafe">
+        <section class="product_section">
         <div class="container">
-            <div class="d-flex justify-content-between cafe_text">
+            <!-- {{-- <div class="d-flex justify-content-between cafe_text">
                 <h2 class="fw-bold">Самые популярные блюда</h2>
                 <a>Перейти к заведениям <img src="/img/bxs_dish.png"></a>
-            </div>
+            </div> --}} -->
                 <div class="d-flex flex-wrap grid gap-4">
                     <div class="cart_cafe cart_product g-col-4">
                         <img src="/img/66e1608c038e458e7185685a45251707.jpg" alt="картинка заведения"> 
