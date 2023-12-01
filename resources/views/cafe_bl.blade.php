b95694e6833f90e977d589befaa469bda79baf12
KirillHorn <122849903+KirillHorn@users.noreply.github.com>
2023-12-01 22:09:44 +0500

    Доделал страницу заведения
    фвфвфвф

--- resources/views/cafe_bl.blade.php
+++ resources/views/cafe_bl.blade.php
@@ -18,19 +18,90 @@
                         <iframe class="iframe_goods" src="https://yandex.ru/map-widget/v1/?um=constructor%3A11744030def0a8feaad6cce670b0946d4bca01fd6116e2522927cde0288642f7&amp;source=constructor"  frameborder="0"></iframe>
                 </div>
 
-                <div class="information_cafe_product">
+                <div class="information_cafe_product d-flex">
                     <div class="cafe_product">
                                 <div class="">
-                                    <h2>Популярное</h2>
-                                         <div class="cafe_product_info">
-                                            <img src="/img/" alt="картинка заведения"> 
+                                    <h2 class="fw-bold">Популярное</h2>
+                                    <div class="d-flex flex-wrap grid gap-4 ">
+                                         <div class="cafe_product_info cart_cafe g-col-4">
+                                            <img src="/img/66e1608c038e458e7185685a45251707.jpg" alt="картинка заведения"> 
                                              <p>Азиатска кухня • ₽</p>
                                             <a>Рамен горящего города</a>
                                         <p class="fw-bold">190 ₽•45 минут</p>
                                       </div>
+                                      <div class="cafe_product_info cart_cafe g-col-4">
+                                            <img src="/img/66e1608c038e458e7185685a45251707.jpg" alt="картинка заведения"> 
+                                             <p>Азиатска кухня • ₽</p>
+                                            <a>Рамен горящего города</a>
+                                        <p class="fw-bold">190 ₽•45 минут</p>
+                                      </div>
+                                      <div class="cafe_product_info cart_cafe g-col-4">
+                                            <img src="/img/66e1608c038e458e7185685a45251707.jpg" alt="картинка заведения"> 
+                                             <p>Азиатска кухня • ₽</p>
+                                            <a>Рамен горящего города</a>
+                                        <p class="fw-bold">190 ₽•45 минут</p>
+                                      </div>
+                                    </div>
                                 </div>
+
+                                <div class="">
+                                    <h2 class="fw-bold">Завтрак</h2>
+                                    <div class="d-flex flex-wrap grid gap-4 ">
+                                         <div class="cafe_product_info cart_cafe g-col-4" id="part1">
+                                            <img src="/img/66e1608c038e458e7185685a45251707.jpg" alt="картинка заведения"> 
+                                             <p>Азиатска кухня • ₽</p>
+                                            <a>Рамен горящего города</a>
+                                        <p class="fw-bold">190 ₽•45 минут</p>
+                                      </div>
+                                      <div class="cafe_product_info cart_cafe g-col-4">
+                                            <img src="/img/66e1608c038e458e7185685a45251707.jpg" alt="картинка заведения"> 
+                                             <p>Азиатска кухня • ₽</p>
+                                            <a>Рамен горящего города</a>
+                                        <p class="fw-bold">190 ₽•45 минут</p>
+                                      </div>
+                                      <div class="cafe_product_info cart_cafe g-col-4">
+                                            <img src="/img/66e1608c038e458e7185685a45251707.jpg" alt="картинка заведения"> 
+                                             <p>Азиатска кухня • ₽</p>
+                                            <a>Рамен горящего города</a>
+                                        <p class="fw-bold">190 ₽•45 минут</p>
+                                      </div>
+                                    </div>
+                                </div>
+
+                                <div class="">
+                                    <h2 class="fw-bold">Обед</h2>
+                                    <div class="d-flex flex-wrap grid gap-4 ">
+                                         <div class="cafe_product_info cart_cafe g-col-4">
+                                            <img src="/img/66e1608c038e458e7185685a45251707.jpg" alt="картинка заведения"> 
+                                             <p>Азиатска кухня • ₽</p>
+                                            <a>Рамен горящего города</a>
+                                        <p class="fw-bold">190 ₽•45 минут</p>
+                                      </div>
+                                      <div class="cafe_product_info cart_cafe g-col-4">
+                                            <img src="/img/66e1608c038e458e7185685a45251707.jpg" alt="картинка заведения"> 
+                                             <p>Азиатска кухня • ₽</p>
+                                            <a>Рамен горящего города</a>
+                                        <p class="fw-bold">190 ₽•45 минут</p>
+                                      </div>
+                                      <div class="cafe_product_info cart_cafe g-col-4">
+                                            <img src="/img/66e1608c038e458e7185685a45251707.jpg" alt="картинка заведения"> 
+                                             <p>Азиатска кухня • ₽</p>
+                                            <a>Рамен горящего города</a>
+                                        <p class="fw-bold">190 ₽•45 минут</p>
+                                      </div>
+                                    </div>
+                                </div>
+
                         </div>
-                        <div class="cafe_product_navigation">
+                        <div class="cafe_product_navigation ">
+                          <ul class="">
+                          <a href="#part1"><li>Популярные</li></a>
+                            <li>Завтрак</li>
+                            <li> Обед</li>
+                            <li>Напитки</li>
+                            <li>Горячие блюда</li>
+                            <li>Салат</li>
+                          </ul>
                         </div>
         </div>
     </section>
