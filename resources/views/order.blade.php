1c5f019b959b28f0535934cd25d31e1c91e70ddc
KirillHorn <kirilltihonov22@gmail.com>
2023-12-04 18:52:08 +0500

    Почти доверстал страницу корзины
    Осталось сделать страницу модератора

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -5,17 +5,41 @@
     <section class="">
         <div class="container">
             <div class="d-flex flex-column">
-                <div class="Delivery">
-                          <h2>Условие доставки</h2>
-                          <div> <button>200руб</button> <button>Самовывоз</button></div>
-                                <div>
-                         <input placeholder="Что-что">
-                         <input>
-                             </div>
-                </div>
-                  <div class="basket">
 
-                  </div>
+                <div class="Delivery">
+                                <div class="d-flex flex-column gap-2 Delivery_info">
+
+                          <h2 class="fw-bold">Условия доставки</h2>
+                          <div class="Delivery_conditions" > <p>200руб</p> </div>
+                                <div class="d-flex flex-column gap-2">
+                                    <div class="d-flex gap-1"> 
+                                        <img src="/img/home.svg">
+                         <input placeholder="Что-что" class="input_adress">
+                                 </div>
+                         <input class="input_commen" placeholder="Комментарий курьеру">
+                             </div>
+                                 <div>
+                                     <div class="d-flex">
+                                        <img src="/img/clock.svg"><h2 class="fw-bold">Время доставки</h2>
+                                    </div>
+                                        <p class="fw-bold">Доставка за 20-30 минут</p>
+
+                                 </div>
+                        </div>
+                </div>
+
+                  <div class="Delivery">  
+                         <div class="basket">
+                              <div>
+                                 <h2>Корзина</h2>           
+                              </div>
+                                    <div class="d-flex flex-column">
+                                        <div class="basket_product d-flex gap-3 align-items-center">
+                                            <img src="/img/66e1608c038e458e7185685a45251707.jpg"> <p class="fw-bold">Рамен горящего города </p> <p>190₽</p>  <div>+ 1 - </div></div>
+                                    </div>
+                        </div>
+                  
+                    </div>
             </div>
         </div>
     </section>
