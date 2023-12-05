e01d5df76dcb97c97c39737f19a9ef18b6c3aa66
KirillHorn <kirilltihonov22@gmail.com>
2023-12-05 13:00:01 +0500

    счётчик для корзины

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -34,8 +34,20 @@
                                  <h2>Корзина</h2>           
                               </div>
                                     <div class="d-flex flex-column">
-                                        <div class="basket_product d-flex gap-3 align-items-center">
-                                            <img src="/img/66e1608c038e458e7185685a45251707.jpg"> <p class="fw-bold">Рамен горящего города </p> <p>190₽</p>  <div>+ 1 - </div></div>
+                                        <div class="basket_product d-flex gap-3 align-items-center justify-content-around">
+                                            <img src="/img/66e1608c038e458e7185685a45251707.jpg"> <p class="fw-bold" >Рамен горящего города </p>  <p id="price">195</p>  
+                                            {{-- <div class="text-center">+ 1 - </div> --}}
+                                            <div class="counter d-flex gap-1 justify-content-evenly align-items-center">
+                                            <input id="down" type="button" class="input_count" value="-">
+                                            <input id="numericUpDown" type="number" value="1" class="count"/>
+                                      
+                                            <input id="up" type="button" class="input_count" value="+">
+                                            </div>
+                                            
+                                        </div>
+                                        <div class="d-flex  justify-content-between align-items-center "> <h2>Сумма корзины</h2> <p id="sum" class="text-center Sum_Button fw-bold">195 </p> </div>
+
+                                        
                                     </div>
                         </div>
                   
@@ -44,4 +56,25 @@
         </div>
     </section>
  
+    <script>
+        let price = document.getElementById("price");
+        let sum = document.getElementById("sum");
+        let numericUpDown = document.getElementById("numericUpDown");
+        let up = document.getElementById("up");
+        let down = document.getElementById("down");
+        up.onclick = () => {
+            numericUpDown.value = (isNaN(numericUpDown.value)) ? 1 : +numericUpDown.value + 1;
+            setSum();
+        };
+        down.onclick = () =>{
+            numericUpDown.value = (numericUpDown.value) > 0 ? +numericUpDown.value - 1 : 0;
+            setSum();
+        }
+ 
+        numericUpDown.oninput = setSum;
+ 
+        function setSum() {
+            sum.innerText = (price.innerText * numericUpDown.value)
+        }
+    </script>
 <x-footer/>
